bd801607c1e3218aa52fca23ada66e02244f82a2
Jamie Gregory <jamiegregsa@gmail.com>
2024-03-22 19:16:32 +0200

    render listen step

--- app/track/[slug]/page.tsx
+++ app/track/[slug]/page.tsx
@@ -1,104 +1,128 @@
 import {
+  fetchTrackBySlug,
+  getResizedArtworkUrl,
+  ITrack,
+} from "@spinamp/spinamp-sdk";
+import {
   FrameButton,
   FrameContainer,
   FrameImage,
-  FrameInput,
   FrameReducer,
   NextServerPageProps,
-  getFrameMessage,
   getPreviousFrame,
   useFramesReducer,
 } from "frames.js/next/server";
 import Link from "next/link";
-import { DEFAULT_DEBUGGER_HUB_URL, createDebugUrl } from "../../debug";
+import { zora } from "viem/chains";
 import { currentURL } from "../../utils";
-import {fetchTrackBySlug, getResizedArtworkUrl, ITrack} from '@spinamp/spinamp-sdk';
+import { createDebugUrl } from "../../debug";
 
+enum Page {
+  HOME,
+  LISTEN,
+}
 
 type State = {
-  // track: ITrack | undefined
+  currentPage: Page;
 };
 
-const initialState = { track: undefined };
+const initialState = { currentPage: Page.HOME };
 
 const reducer: FrameReducer<State> = (state, action) => {
-  console.log("reducer got action", action)
+  console.log("reducer got action", state, action);
+  const buttonIndex = action.postBody?.untrustedData.buttonIndex;
+
+  console.log("~~~~~~~~~~~~~~~~~` got button index", buttonIndex);
+
+  let page = state.currentPage;
+
+  if (state.currentPage === Page.HOME) {
+    if (buttonIndex === 1) {
+      page = Page.LISTEN;
+    }
+  }
+
   return {
-    // track: {
-    //   title: "",
-    //   image: "",
-    //   artist: "",
-    //   spinampUrl: ""
-    // }
+    currentPage: page,
   };
 };
 
 // This is a react server component only
-export default async function Track({ searchParams, params }: NextServerPageProps) {
-  console.log("render Track")
-  if (!params.slug) {
-    console.log("bad payload")
-    throw new Error("Invalid frame payload");
+export default async function Track({
+  searchParams,
+  params,
+}: NextServerPageProps) {
+  const slug = params.slug;
+  if (!slug) {
+    throw new Error("invalid payload: no slug");
   }
-  const url = currentURL(`/track/${params.slug}`);
+  const url = currentURL("/examples/mint-button");
   const previousFrame = getPreviousFrame<State>(searchParams);
+  const [state] = useFramesReducer<State>(reducer, initialState, previousFrame);
 
-  console.log("got previous frame", previousFrame)
+  const track = await fetchTrackBySlug(slug);
 
-  const frameMessage = await getFrameMessage(previousFrame.postBody, {
-    hubHttpUrl: DEFAULT_DEBUGGER_HUB_URL,
-  });
-
-  if (frameMessage && !frameMessage?.isValid) {
-    throw new Error("Invalid frame payload");
+  if (state.currentPage === Page.HOME) {
+    // then, when done, return next frame
+    return (
+      <div>
+        Mint button example <Link href={createDebugUrl(url)}>Debug</Link>
+        <FrameContainer
+          pathname={`/track/${slug}`}
+          postUrl="/track/loaded/frames"
+          state={state}
+          previousFrame={previousFrame}
+        >
+          <FrameImage aspectRatio="1.91:1">
+            <div tw="w-full h-full bg-slate-700 text-white justify-center items-center flex flex-col">
+              <div tw="flex flex-row"></div>
+              <div tw="flex flex-row">{track?.title}</div>
+              <div tw="flex flex-row">{track?.artist.name}</div>
+              <div tw="flex flex-row">{state.currentPage}</div>
+            </div>
+          </FrameImage>
+          <FrameButton>Listen</FrameButton>
+          <FrameButton
+            action="link"
+            target={`https://app.spinamp.xyz/track/${params.slug}`}
+          >
+            open
+          </FrameButton>
+        </FrameContainer>
+      </div>
+    );
   }
 
-  const [state, dispatch] = useFramesReducer<State>(
-    reducer,
-    initialState,
-    previousFrame
-  );
+  if (state.currentPage === Page.LISTEN) {
+    // send notification
 
-  // Here: do a server side side effect either sync or async (using await), such as minting an NFT if you want.
-  // example: load the users credentials & check they have an NFT
-    const track = await fetchTrackBySlug(params.slug);
-
-  console.log("track", track)
-
-  console.log("info: state is:", state);
-
-  // then, when done, return next frame
-  return (
-    <div className="p-4">
-      <Link href={createDebugUrl(url)} className="underline">
-        Debug
-      </Link>
-      <FrameContainer
-        postUrl={url.toString()}
-        pathname="/"
-        state={state}
-        previousFrame={previousFrame}
-      >
-        <FrameImage aspectRatio="1.91:1">
-          <div tw="w-full h-full bg-slate-700 text-white justify-center items-center flex flex-col">
-          <div tw="flex flex-row">
-               {/* <img src={getResizedArtworkUrl(track?.lossyArtworkUrl, 50)} /> */}
+    // then, when done, return next frame
+    return (
+      <div>
+        Mint button example <Link href={createDebugUrl(url)}>Debug</Link>
+        <FrameContainer
+          pathname={`/track/${slug}`}
+          postUrl="/track/loaded/frames"
+          state={state}
+          previousFrame={previousFrame}
+        >
+          <FrameImage aspectRatio="1.91:1">
+            <div tw="w-full h-full bg-slate-700 text-white justify-center items-center flex flex-col">
+              <div tw="flex flex-row">download spinamp to listen</div>
+              <div tw="flex flex-row">{track?.title}</div>
+              <div tw="flex flex-row">{track?.artist.name}</div>
+              <div tw="flex flex-row">{state.currentPage}</div>
             </div>
-            <div tw="flex flex-row">
-               {track?.title}
-            </div>
-            <div tw="flex flex-row">
-               {track?.artist.name}
-            </div>
-          </div>
-        </FrameImage>
-        <FrameButton action="post" target={url.toString()}>
-          Listen
-        </FrameButton>
-        <FrameButton action="link" target={`https://app.spinamp.xyz/track/${params.slug}`}>
-          open
-        </FrameButton>
-      </FrameContainer>
-    </div>
-  );
+          </FrameImage>
+          <FrameButton>collect</FrameButton>
+          <FrameButton
+            action="link"
+            target={`https://app.spinamp.xyz/track/${params.slug}`}
+          >
+            open
+          </FrameButton>
+        </FrameContainer>
+      </div>
+    );
+  }
 }
