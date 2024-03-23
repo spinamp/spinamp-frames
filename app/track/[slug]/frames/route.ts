e89d4b96427e3bf086c7f1db4d19d019b3966a90
musnit <musnit@gmail.com>
2024-03-22 22:34:51 -0700

    Set caching

--- app/track/[slug]/frames/route.ts
+++ app/track/[slug]/frames/route.ts
@@ -1,1 +1,7 @@
-export { POST } from "frames.js/next/server";
+import { POST as POSTNext } from "frames.js/next/server";
+import { NextRequest, NextResponse } from 'next/server';
+
+export function POST(req: NextRequest, res: NextResponse) {
+  res.headers.set("Cache-Control", "max-age=180");
+  return POSTNext(req, res);
+}
