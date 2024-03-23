f3107b6240d56ca58b070dc65dff49d85914c8b5
musnit <musnit@gmail.com>
2024-03-22 22:42:27 -0700

    fix cache bug

--- app/track/[slug]/frames/route.ts
+++ app/track/[slug]/frames/route.ts
@@ -1,7 +1,8 @@
 import { POST as POSTNext } from "frames.js/next/server";
 import { NextRequest, NextResponse } from 'next/server';
 
-export function POST(req: NextRequest, res: NextResponse) {
-  res.headers.set("Cache-Control", "max-age=180");
-  return POSTNext(req, res);
+export async function POST(req: NextRequest, res: typeof NextResponse) {
+  const nextResponse = await POSTNext(req, res);
+  nextResponse.headers.set("Cache-Control", "max-age=180");
+  return nextResponse;
 }
