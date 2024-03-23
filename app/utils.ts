1b37c70ad54956440761b369d3802ce1d61a2011
Jamie Gregory <jamiegregsa@gmail.com>
2024-03-23 02:37:25 +0200

    fetch spinamp user id

--- app/utils.ts
+++ app/utils.ts
@@ -1,5 +1,6 @@
 import { Wallet } from "ethers";
 import { headers } from "next/headers";
+import { GraphQLClient, gql } from "graphql-request";
 
 export function currentURL(pathname: string): URL {
   const headersList = headers();
@@ -79,4 +80,39 @@ async function signMessage(message: string) {
   const wallet = new Wallet(pk);
 
   return wallet.signMessage(message);
+}
+
+export async function getSpinampUserId(
+  addresses: string[]
+): Promise<string | undefined> {
+  const graphqlApi = new GraphQLClient(process.env.SPINAMP_GRAPHQL_ENDPOINT!);
+  console.log(
+    "search addresses",
+    addresses,
+    "at",
+    process.env.SPINAMP_GRAPHQL_ENDPOINT!
+  );
+  const response: any = await graphqlApi.request(
+    gql`
+      query UserIdQuery($addresses: [String!]) {
+        allAddresses(filter: { id: { in: $addresses } }) {
+          nodes {
+            userId
+          }
+        }
+      }
+    `,
+    {
+      addresses,
+    }
+  );
+
+  const nodes = response.allAddresses.nodes;
+
+  if (nodes.length === 0) {
+    return undefined;
+  }
+
+  // TODO: what should we do if there is more than one spindexer user??
+  return nodes.map((node: any) => node.userId).at(0);
 }
