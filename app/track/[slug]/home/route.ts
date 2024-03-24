ad8cebfeacf54a9897f779c216085657a4611460
Jamie Gregory <jamiegregsa@gmail.com>
2024-03-24 10:06:08 +0200

    add logs

--- app/track/[slug]/home/route.ts
+++ app/track/[slug]/home/route.ts
@@ -1,8 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest): Promise<NextResponse> {
+  console.log("~~~~ in post redirect ~~~~~~~~~~~");
   const queryParams = new URL(req.url).searchParams;
+  console.log("got params", queryParams);
   const slug = queryParams.get("slug");
+  console.log("got slug", slug);
 
   const url = new URL(req.url);
 
