cc58712be8bb1536724d7d49c62bc2186b293905
Jamie Gregory <jamiegregsa@gmail.com>
2024-03-24 10:16:21 +0200

    remove logs

--- app/track/[slug]/home/route.ts
+++ app/track/[slug]/home/route.ts
@@ -1,14 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest): Promise<NextResponse> {
-  console.log("~~~~ in post redirect ~~~~~~~~~~~");
   const reqUrl = new URL(req.url);
-  console.log("got url", reqUrl);
-  console.log("split path", reqUrl.pathname.split("/"));
   const slug = reqUrl.pathname.split("/")[2];
-  console.log("got slug", slug);
 
-  const url = new URL(req.url);
-
-  return NextResponse.redirect(`${url.origin}/track/${slug}`);
+  return NextResponse.redirect(`${reqUrl.origin}/track/${slug}`);
 }
