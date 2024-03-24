d8a0870f62d3d2168af6b85bc587cadf2984589e
Jamie Gregory <jamiegregsa@gmail.com>
2024-03-24 10:13:45 +0200

    get slug from path

--- app/track/[slug]/home/route.ts
+++ app/track/[slug]/home/route.ts
@@ -2,9 +2,10 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest): Promise<NextResponse> {
   console.log("~~~~ in post redirect ~~~~~~~~~~~");
-  const queryParams = new URL(req.url).searchParams;
-  console.log("got params", queryParams);
-  const slug = queryParams.get("slug");
+  const reqUrl = new URL(req.url);
+  console.log("got url", reqUrl);
+  console.log("split path", reqUrl.pathname.split("/"));
+  const slug = reqUrl.pathname.split("/")[2];
   console.log("got slug", slug);
 
   const url = new URL(req.url);
